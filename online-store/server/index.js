045ea1e2720fed7f4fcdceabe683c9078b85d0b9
annamitrofanova <22amitrofa@gmail.com>
2023-12-05 04:59:21 +0300

    update controllers

--- online-store/server/index.js
+++ online-store/server/index.js
@@ -1,20 +1,21 @@
 require('dotenv').config()
 
 const express = require('express')
-
 const sequelize = require('./db')
-
 const models = require('./models/models')
-
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static'))) //изображения там
+app.use(fileUpload({}))
 //запросы из браузера
 //app.get('/', (reg, res) => {
    // res.status(200).json({message:'WORKING!!!'})
