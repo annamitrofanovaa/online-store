03913761b67b94f05ab8298452f479dae29f166d
NadyaPronina <nadya.cats@gmail.com>
2023-12-22 00:22:26 +0300

    corses

--- online-store/server/routes/userRouter.js
+++ online-store/server/routes/userRouter.js
@@ -3,6 +3,13 @@ const router = new Router()
 const userController = require('../controllers/userController')
 const authMiddleware = require('../middleware/authMiddleware')
 
+router.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Укажите нужный домен или '*' для доступа с любых доменов
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    res.status(204).send();
+  });
+
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
 router.get('/auth', authMiddleware, userController.check)
